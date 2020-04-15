934e577b274c1e9ed6564ee89bdcfa33ae98ae17
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-15 00:51:53 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(Squawk = "Squawk!")
+def parrot(squawk = "Squawk!")
   puts "Squawk!"
   return "Squawk!"
 end
